df5685091070667d04626f0686c83a9e6fe3b5eb
Abasgulu <aliyevabasgulu@gmail.com>
2024-03-14 03:09:00 -0700

    Initial commit

---
+++ src/RectangleDemo.java
@@ -0,0 +1,10 @@
+//    }
+}
+public class RectangleDemo {
+    public static void main(String[] args) throws I0Exception{
+        RectangleArea ra =new RectangleArea();
+        ra.getData();
+        ra.ComputeField();
+        ra.fieldDisplay();
+    }
+}
